b8ef7c2d9dffe6e2e477f0b479b453dc4dfbf2bb
Liam Sullivan <30022060@student.toiohomai.ac.nz>
2019-07-31 15:31:00 +1200

    yes

---
+++ EncapQuestions/EncapQuestions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapQuestions
+{
+    //PRACTICE QUESTIONS 2 Task 1 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 0;
+            int width = 0;
+            int length = 0;
+
+            height = GetData(height, "Height");
+            width = GetData(width, "Width");
+            length = GetData(length, "Length");
+
+            Box b1 = new Box(height, width, length);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(b1.GetSurfaceArea());
+            Console.ReadLine();
+
+        }
+
+
+        static int GetData(int number, string info)
+        {
+            bool check = true;
+            bool confirm = false;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"Please enter {info} of the Box: ");
+                confirm = int.TryParse(Console.ReadLine(), out number);
+                do
+                {
+                    if (!confirm)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("ERROR. NUMBER NOT VALID. PLEASE TRY AGAIN.");
+                        confirm = int.TryParse(Console.ReadLine(), out number);
+                    }
+                } while (!confirm);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"{info} has been set.\n\n");
+                break;
+
+            } while (check);
+             return number;   
+        }
+      
+      class Box
+      {
+            private int Height { get; set; }
+            private int Width { get; set; }
+            private int Length { get; set; }
+
+            public Box(int _height, int _width, int _length)
+            {
+                Height = _height;
+                Width = _width;
+                Length = _length;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("***BOX HAS BEEN CREATED***\n\n");
+            }
+         public string GetSurfaceArea()
+            {
+                return $"Surface Area of Box: {2 * (Height * Width) + 2 * (Height * Length) + 2 * (Width * Length)}";
+            }
+
+      }
+
+    }
+}
---
+++ EncapQuestions2/EncapQuestions2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapQuestions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Console.WriteLine("\t***PROGRAM START***");
+            Console.Write("Please enter the student's name: ");
+            string name = Console.ReadLine();
+            CheckString(name);
+            Console.WriteLine("NAME HAS BEEN ACCEPTED.");
+            Console.ReadLine();
+
+        }
+
+        static string CheckString(string input)
+        {
+            do
+            {
+                if (input == "" || input == null)
+                {
+                    Console.WriteLine("ERROR. NAME INVALID. PLEASE TRY AGAIN.");
+                }
+            } while (input == "");
+            return input;
+        }
+        class Student
+        {
+            private string name { get; set; }
+            private string email { get; set; }
+            private string address { get; set; }
+            private string user { get; set; }
+            private string password { get; set; }
+            private string academic { get; set; }
+            private string emergency { get; set; }
+
+            int[] results = new int[4];
+            public int[] Results
+            {
+                get { return results; }
+                set { results = value; }
+            }
+
+            public string StudentInfo()
+            {
+                double average = 0;
+                foreach (int x in Results)
+                    average += x;
+                average = average / Results.Length;
+
+                return $"Name: {name}\nResult 1: {Results[0]}\nResult 2:{Results[1]} \nResult 3:{Results[2]} \nResult 4:{Results[3]}\nResult Average: {average}";
+
+            }
+
+        }
+    }
+}
